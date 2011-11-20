5abdfad45e6ce366514ee906c3d7d5a3f9d94582
Pepijn de Vos <pepijndevos@gmail.com>
2011-11-20 21:37:08 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject armagedom "0.0.1-SNAPSHOT"
+(defproject armagedom "1.0.0-SNAPSHOT"
   :description "The epic battle on the final day of XML.
                 A Clojure DSL similar to Hiccup to generate XML."
   :warn-on-reflection true
