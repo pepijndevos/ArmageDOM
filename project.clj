94fcdd61a237472a05a117e258c6fa852ba88ed9
Pepijn de Vos <pepijndevos@gmail.com>
2011-11-20 15:17:37 +0100

    update to clojure 1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject armagedom "0.0.1-SNAPSHOT"
   :description "The epic battle on the final day of XML.
                 A Clojure DSL similar to Hiccup to generate XML."
-  :dependencies [[clojure "1.2.0"]])
+  :dependencies [[clojure "1.3.0"]])
